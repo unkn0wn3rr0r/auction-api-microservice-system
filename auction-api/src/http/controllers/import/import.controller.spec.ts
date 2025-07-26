8c4aa0823b33fa9dc0de40fe2add98a54d130076
kstoyanov <kstoyanovv@abv.bg>
2025-07-26 21:38:09 +0300

    fix and use fs/promises module

--- auction-api/src/http/controllers/import/import.controller.spec.ts
+++ auction-api/src/http/controllers/import/import.controller.spec.ts
@@ -1,15 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ImportController } from './import.controller';
 import { CsvImportService } from 'src/core/services/csv-import/csv-import.service';
-import { readFile } from 'fs';
-import { promisify } from 'util';
+import { readFile } from 'fs/promises';
 import { AuthTransportationService } from 'src/core/services/transport/auth.transportation.service';
 import { HttpModule } from '@nestjs/axios';
 
-jest.mock('fs', () => ({
+jest.mock('fs/promises', () => ({
     readFile: jest.fn(),
 }));
-const mockReadFileAsync = promisify(readFile) as jest.Mock;
 
 describe('ImportController', () => {
     let controller: ImportController;
@@ -42,13 +40,10 @@ describe('ImportController', () => {
 
     it('should return success message and count when CSV is imported', async () => {
         const fakeBuffer = Buffer.from('fake csv content');
-        mockReadFileAsync.mockImplementation((_, callback) => {
-            callback(null, fakeBuffer);
-        });
+        (readFile as jest.Mock).mockResolvedValue(fakeBuffer);
         mockCsvImportService.importCsvData.mockResolvedValue(3);
 
         const result = await controller.importCsv();
-
         expect(result).toEqual({
             message: 'Data import complete',
             importedCount: 3,
@@ -58,12 +53,9 @@ describe('ImportController', () => {
     });
 
     it('should return error message when import fails', async () => {
-        mockReadFileAsync.mockImplementation((_, callback) => {
-            callback(new Error('File read failed'), null);
-        });
+        (readFile as jest.Mock).mockRejectedValue(new Error('File read failed'));
 
         const result = await controller.importCsv();
-
         expect(result).toEqual({
             message: 'File read failed',
             importedCount: 0,
--- auction-api/src/http/controllers/import/import.controller.ts
+++ auction-api/src/http/controllers/import/import.controller.ts
@@ -4,25 +4,11 @@ import {
     UseGuards,
 } from '@nestjs/common';
 import { CsvImportService } from 'src/core/services/csv-import/csv-import.service';
-import { readFile } from 'fs';
+import { readFile } from 'fs/promises';
 import { join } from 'path';
-import { promisify } from 'util';
 import { cwd } from 'process';
 import { AuthGuard } from 'src/http/guards/auth.guards';
 
-const readFileAsync = promisify(readFile);
-
-/*
-    TODO:
-    
-    The drawback of that approach is that, the names here are hardcoded, so if:
-    - the folder is placed somewhere else - that breaks
-    - the folder name is changes - that breaks
-    - the csv data filename is changed - that breaks
-    - the csv data is moved somewhere else or is remove that breaks
-
-    Of course this would be a problem if we frequently change folder/file names.
-*/
 const CSV_RELATIVE_PATH = join(cwd(), 'files', 'auction_data.csv');
 
 @Controller('/import')
@@ -33,7 +19,7 @@ export class ImportController {
     @Post('/csv')
     async importCsv(): Promise<{ message: string; importedCount: number }> {
         try {
-            const buff = await readFileAsync(CSV_RELATIVE_PATH);
+            const buff = await readFile(CSV_RELATIVE_PATH);
             const importedCount = await this.service.importCsvData(buff);
             return { message: 'Data import complete', importedCount };
         } catch (error) {
